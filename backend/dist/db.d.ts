70a5c78959fd8a0ed8943a6d9f434443f58bed3d
umaa23 <uma.sharma@colligence.in>
2026-01-18 22:53:37 +0530

    Remove dist folder and add to gitignore

--- backend/dist/db.d.ts
+++
@@ -1,8 +0,0 @@
-import sqlite3 from 'sqlite3';
-export declare const db: sqlite3.Database;
-export declare const initializeDatabase: () => Promise<void>;
-export declare const runQuery: <T = any>(sql: string, params?: any[]) => Promise<T>;
-export declare const runInsert: (sql: string, params?: any[]) => Promise<{
-    lastID: number;
-}>;
-//# sourceMappingURL=db.d.ts.map
